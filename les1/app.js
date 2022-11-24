3f4c5c5a7edfd0ba822d62a2db5b7d88db11f194
keith <25635@ma-web.nl>
2022-11-24 10:27:49 +0100

    les1

---
+++ les1/app.js
@@ -0,0 +1,26 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!");
+        //code gaat hier
+        let appNaam = "K100"
+        let versienummer = 0.9
+        let versiedatum = "11/24/2022"
+        let autheur = "string"
+        let copyright = "string"
+        let distributeur = "string"
+        let darkmode = "bool"
+        
+        console.log(appNaam = "k100")
+        console.log(versienummer = 0.9)
+        console.log(versiedatum = "11/24/2022")
+        console.log(autheur = "string")
+        console.log(copyright = "string")
+        console.log(distributeur = "string")
+        console.log(darkmode = "bool")
+    }
+}
+
+let app = new App();
+app.runApplication();
